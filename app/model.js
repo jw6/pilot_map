1e5334ed97194f79ec15648cc1520cb6a49b053e
Jay Wang <jaywang007@yahoo.com>
2017-04-11 01:19:54 -0400

    modified model and routes and add config

--- app/model.js
+++ app/model.js
@@ -1,6 +1,8 @@
+// Pulls Mongoose dependency for creating schemas
 var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
 
+// Creates a User Schema. This will be the basis of how user data is stored in the db
 var UserSchema = new Schema({
     username: {type: String, required: true},
     gender: {type: String, required: true},
@@ -12,6 +14,7 @@ var UserSchema = new Schema({
     updated_at: {type: Date, default: Date.now}
 });
 
+// Sets the created_at parameter equal to the current time
 UserSchema.pre('save', function(next){
     now = new Date();
     this.updated_at = now;
@@ -21,6 +24,8 @@ UserSchema.pre('save', function(next){
     next();
 });
 
+// Indexes this schema in geoJSON format (critical for running proximity searches)
 UserSchema.index({location: '2dsphere'});
 
+// Exports the UserSchema for use elsewhere. Sets the MongoDB collection to be used as: "scotch-user"
 module.exports = mongoose.model('scotch-user', UserSchema);
